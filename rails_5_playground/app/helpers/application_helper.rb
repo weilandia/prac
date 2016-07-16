e8200bd06075f9a233a0f108c80a80a91935bea7
Nick Weiland <nickweiland@gmail.com>
2016-07-15 21:07:15 -0700

    rails 5 playground

--- rails_5_playground/app/helpers/application_helper.rb
+++ rails_5_playground/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "rails 5 playground"
+    page_title.empty? ? base_title : base_title + " | " + page_title
+  end
 end
--- rails_5_playground/test/controllers/static_pages_controller_test.rb
+++ rails_5_playground/test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "home"
+    assert_select "title", "rails 5 playground | home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "help"
+    assert_select "title", "rails 5 playground | help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "about"
+    assert_select "title", "rails 5 playground | about"
   end
 end
